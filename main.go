efde4f4bb5444b74c4966a710a67aa57dcc0dcfb
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2023-05-12 21:54:33 +0100

    more changes

--- main.go
+++ main.go
@@ -34,41 +34,19 @@ func main() {
 	content := []byte("# Hola\n* This is a file committed via Github's API\n")
 	message := "hopefully signed commit"
 
-	createBranch
+	branchRef, err := createBranch(ctx, client, *branch)
+	if err != nil {
+		log.Fatalf("error creating branch %s", err.Error())
+	}
 
-	// create the branch and get it's reference
-	client.Git.CreateRef(ctx, *owner, *repo, &github.Reference{})
-
-	// 	owner: 'OWNER',
-	//   repo: 'REPO',
-	//   message: 'my commit message',
-	//   author: {
-	//     name: 'Mona Octocat',
-	//     email: 'octocat@github.com',
-	//     date: '2008-07-09T16:13:30+12:00'
-	//   },
-	//   parents: [
-	//     '7d1b31e74ee336d15cbd21741bc88a537ed063a0'
-	//   ],
-	//   tree: '827efc6d56897b048c772eb4087f854f46256132',
-	//   signature: '-----BEGIN PGP SIGNATURE-----\n\niQIzBAABAQAdFiEESn/54jMNIrGSE6Tp6cQjvhfv7nAFAlnT71cACgkQ6cQjvhfv\n7nCWwA//XVqBKWO0zF+bZl6pggvky3Oc2j1pNFuRWZ29LXpNuD5WUGXGG209B0hI\nDkmcGk19ZKUTnEUJV2Xd0R7AW01S/YSub7OYcgBkI7qUE13FVHN5ln1KvH2all2n\n2+JCV1HcJLEoTjqIFZSSu/sMdhkLQ9/NsmMAzpf/iIM0nQOyU4YRex9eD1bYj6nA\nOQPIDdAuaTQj1gFPHYLzM4zJnCqGdRlg0sOM/zC5apBNzIwlgREatOYQSCfCKV7k\nnrU34X8b9BzQaUx48Qa+Dmfn5KQ8dl27RNeWAqlkuWyv3pUauH9UeYW+KyuJeMkU\n+NyHgAsWFaCFl23kCHThbLStMZOYEnGagrd0hnm1TPS4GJkV4wfYMwnI4KuSlHKB\njHl3Js9vNzEUQipQJbgCgTiWvRJoK3ENwBTMVkKHaqT4x9U4Jk/XZB6Q8MA09ezJ\n3QgiTjTAGcum9E9QiJqMYdWQPWkaBIRRz5cET6HPB48YNXAAUsfmuYsGrnVLYbG+\nUpC6I97VybYHTy2O9XSGoaLeMI9CsFn38ycAxxbWagk5mhclNTP5mezIq6wKSwmr\nX11FW3n1J23fWZn5HJMBsRnUCgzqzX3871IqLYHqRJ/bpZ4h20RhTyPj5c/z7QXp\neSakNQMfbbMcljkha+ZMuVQX1K9aRlVqbmv3ZMWh+OijLYVU2bc=\n=5Io4\n-----END PGP SIGNATURE-----\n',
-	//   headers: {
-	//     'X-GitHub-Api-Version': '2022-11-28'
-	//   }
-	// 	client.Git.CreateCommit(ctx, "BolajiOlajide", *repo, &github.Commit{
-	// Parents: [,
-	// 	})
-	// client.PullRequests.Create(ctx, "BolajiOlajide", *repo, &github.NewPullRequest{})
-
-	// create a new private repository named "foo"
-	// repo := &github.Repository{
-	// 	Name:    github.String(repo),
-	// 	Private: github.Bool(false),
-	// }
-	// repo.
-	fmt.Println("token is ", *token)
-	fmt.Println("repo is ", repo)
-	fmt.Println("branch is ", branch, client)
+	_, _, err = client.Repositories.CreateFile(ctx, *owner, *repo, path, &github.RepositoryContentFileOptions{
+		Message: github.String(message),
+		Content: content,
+		Branch:  github.String(*branch),
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func createBranch(ctx context.Context, client *github.Client, branch string) (*github.Reference, error) {
